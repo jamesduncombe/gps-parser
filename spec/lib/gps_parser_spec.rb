907b71e4fa3027078eb1882fd20839a270c1165f
James Duncombe <james@jamesduncombe.com>
2012-11-04 12:27:30 +0100

    Updated tests with let()

--- spec/lib/gps_parser_spec.rb
+++ spec/lib/gps_parser_spec.rb
@@ -1,5 +1,3 @@
-
-
 require 'rspec'
 require_relative '../../lib/gps_parser.rb'
 
@@ -22,10 +20,10 @@ describe 'GPS Parser' do
   describe 'Time Parser' do
 
     it 'should convert a GPS time into a proper Ruby time object' do
-      @f = File.open 'test_gps_data.txt'
-      result = GPS::parse_time @f.ctime, '175055.354'
-      result.should be_an_instance_of Time
-      @f.close
+      File.open 'test_gps_data.txt' do |f|
+        result = GPS::parse_time f.ctime, '175055.354'
+        result.should be_an_instance_of Time
+      end
     end
 
   end
@@ -33,8 +31,8 @@ describe 'GPS Parser' do
   describe GPS::Record do
 
     # stub out a new instance of GPS::Record
-    before do
-      @record = GPS::Record.new(
+    let(:record) do
+      GPS::Record.new(
         :time       => Time.now,
         :longitude  => 2.23,
         :latitude   => 3.12,
@@ -47,33 +45,30 @@ describe 'GPS Parser' do
       lambda { GPS::Record.new.should raise_error ArgumentError }
     end
 
-    it 'should return an instance of GPS::Record' do
-      @record.should be_an_instance_of GPS::Record
-    end
+    it { record.should be_an_instance_of GPS::Record }
 
-    it 'should return an instance of time when queried' do
-      @record.time.should be_an_instance_of Time
-    end
+    it { record.time.should be_an_instance_of Time }
 
   end
 
   describe GPS::Parser do
 
-    before { @parser = GPS::Parser.new }
+    let(:parser) { GPS::Parser.new }
+    let(:load_gps_strings) { parser.load_gps_strings 'test_gps_data.txt' }
 
     it 'should initialize with a bare array of GPS records' do
-      @parser.records.should be_an Array
+      parser.records.should be_an Array
     end
 
     it 'should be able to load GPS data from a text file' do
-      @parser.load_gps_strings 'test_gps_data.txt'
-      @parser.instance_variable_get(:@f).should be_a File
+      load_gps_strings
+      parser.instance_variable_get(:@f).should be_a File
     end
 
     it 'should be able to parse the GPS strings into something useful' do
-      @parser.load_gps_strings 'test_gps_data.txt'
-      @parser.parse_gps
-      @parser.records.first.should be_an_instance_of GPS::Record
+      load_gps_strings
+      parser.parse_gps
+      parser.records.first.should be_an_instance_of GPS::Record
     end
 
   end
